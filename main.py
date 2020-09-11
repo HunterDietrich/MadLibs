6f4f41b7ae237d28f6de763bb54c72e10e0a1fd7
HunterDietrich <70781742+HunterDietrich@users.noreply.github.com>
2020-09-11 23:42:51 +0000

    Added Code

--- main.py
+++ main.py
@@ -2,4 +2,22 @@ print("Welcome to MADLIBS")
 
 song = input("Enter a song.")
 
-print(f"Sometimes i play the song {song})
+print(f"Sometimes I play the song {song} when I eat waffles")
+
+Toppings = input("Enter a topping.")
+
+print(f"I eat {Toppings} with my waffles.")
+
+instrument = input("Enter a instrument.")
+
+print(f"Did I mention I did all of this while playing the {instrument}while showering?")
+
+soft = input("Enter a soft thing.")
+
+print(f"When I get out of the shower I dry myself down with a {soft} and dry my hair")
+
+game = input("Enter a game")
+
+print(f"And after im done I play {game} all day")
+
+print(f"Sometimes I play the song {song} when I eat waffles. I eat {Toppings} with my waffles. Did I mention I did all of this while playing the {instrument} while showering? When I get out of the shower I dry myself down with a {soft} and dry my hair too, and after im done I play {game} all day")
